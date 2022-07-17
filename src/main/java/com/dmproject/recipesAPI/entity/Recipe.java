5035b99ca172b39e6b84b8fe25e7f91dccfa4933
David Morales <david.dmg89@gmail.com>
2022-07-17 16:15:40 +0200

    update Recipe & add atribute category & add annotation Column

--- src/main/java/com/dmproject/recipesAPI/entity/Recipe.java
+++ src/main/java/com/dmproject/recipesAPI/entity/Recipe.java
@@ -15,24 +15,35 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String title;
 
+    @Column
     private String description;
 
+    @Column
     private String timeCook;
 
+    @Column
     private Integer serves;
 
+    @Column
     private String imageUrl;
 
+    @Column
     private String difficulty;
 
+    @Column
     private String ingredients;
 
+    @Column
     private String instruction;
 
+    @Column
+    private String category;
 
-    public Recipe(Long id, String title, String description, String timeCook, Integer serves, String imageUrl, String difficulty, String ingredients, String instruction) {
+
+    public Recipe(Long id, String title, String description, String timeCook, Integer serves, String imageUrl, String difficulty, String ingredients, String instruction, String category) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -42,6 +53,7 @@ public class Recipe {
         this.difficulty = difficulty;
         this.ingredients = ingredients;
         this.instruction = instruction;
+        this.category = category;
     }
 
 
