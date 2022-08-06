dab35b7adc8fc2306ec9d1bb1b91f545f63797ab
David Morales <david.dmg89@gmail.com>
2022-08-06 23:51:25 +0200

    Add Relations User

--- src/main/java/com/dmproject/recipesAPI/entity/User.java
+++ src/main/java/com/dmproject/recipesAPI/entity/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -24,10 +25,22 @@ public class User {
     @Column
     private String password;
 
-    public User(Long id, String email, String name, String password) {
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLES",
+            joinColumns = {
+                    @JoinColumn(name = "USER_ID")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID") })
+    private Set<Role> roles;
+
+
+
+    public User(Long id, String email, String name, String password, Set<Role> roles) {
         this.id = id;
         this.email = email;
         this.name = name;
         this.password = password;
+        this.roles =  roles;
     }
 }
